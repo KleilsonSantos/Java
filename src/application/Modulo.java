d9cd30cb2d023404f52299fb73d57a2a77ed5cc8
KleilsonSantos <kdsdesign1@gmail.com>
2020-06-22 16:42:29 -0300

    Criando Classes de Operacões Aritméticas Básicas

---
+++ src/application/Modulo.java
@@ -0,0 +1,29 @@
+package application;
+
+public class Modulo {
+
+	public static void main(String[] args) {
+
+		// Declarando variaveis x e y, do tipo int.
+		int x = 50;
+		int y = 10;
+
+		/*
+		 * Declarando variavel result do tipo int, recebendo o valor da operação
+		 * realizada.
+		 */
+		int result = (x % y);
+
+		/* Imprimindo o valor da variavel */
+		System.out.println("Valor de x = " + x);
+		System.out.println("Valor de y = " + y);
+		System.out.println("===============");
+
+		/*
+		 * Imprimindo o valor do resto da divisão de x por y na variavel -> result concatenando com string.
+		 */
+		System.out.println("Resultado de ( x % y ) = " + result);
+
+	}
+
+}
---
+++ src/application/Subtracao.java
@@ -0,0 +1,28 @@
+package application;
+
+public class Subtracao {
+
+	public static void main(String[] args) {
+
+		// Declarando variaveis x e y, do tipo int.
+		int x = 50;
+		int y = 10;
+
+		/*
+		 * Declarando variavel result do tipo int, recebendo o valor da operação
+		 * realizada.
+		 */
+		int result = (x - y);
+
+		/* Imprimindo o valor da variavel */
+		System.out.println("Valor de x = " + x);
+		System.out.println("Valor de y = " + y);
+		System.out.println("===============");
+
+		/*
+		 * Imprimindo o valor da variavel -> result concatenando com string.
+		 */
+		System.out.println("Resultado de ( x - y ) = " + result);
+	}
+
+}
